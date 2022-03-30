065fcd3ee0b4ce04c8082a5ab389b590558e4941
Matt-Salamon <2000mattis@gmail.com>
2022-03-30 16:48:33 +0100

    Clean up

--- MuonsDetector.ino
+++ MuonsDetector.ino
@@ -16,16 +16,18 @@ void setup() {
   setSyncProvider(getTeensy3Time);
 
   Serial.begin(115200);
-  while (!Serial);  // Wait for Arduino Serial Monitor to open
-  delay(100);
+  // Wait for Arduino Serial Monitor to open
+  while (!Serial) {
+    delay(100);
+  }
 
   // SET THE CORRECT TIME
-  setTime(16, 14, 0, 30, 3, 2022);
+  // setTime(16, 14, 0, 30, 3, 2022);
  
   if (timeStatus()!= timeSet) {
-  Serial.println("Unable to sync with the RTC");
+    Serial.println("Unable to sync with the RTC");
   } else {
-  Serial.println("RTC has set the system time");
+    Serial.println("RTC has set the system time");
   }
 
   // Set up the sd card
@@ -57,7 +59,7 @@ void setup() {
   } else {
     // if the file didn't open, print an error:
     Serial.println("1 error opening test.txt");
-    }
+  }
 
   // How to overwrite the previous contents?!
  
@@ -80,9 +82,9 @@ void loop() {
   }
 
   if (millis() - lastRefreshTime >= REFRESH_INTERVAL) {
-        lastRefreshTime += REFRESH_INTERVAL;
-        writeDataToSD(cps);
-        cps = 0;
+    lastRefreshTime += REFRESH_INTERVAL;
+    writeDataToSD(cps);
+    cps = 0;
   }
     
 }
@@ -93,31 +95,30 @@ void writeDataToSD(String cps) {
   myFile = SD.open("test.txt", FILE_WRITE); //Also O_WRITE works
   // if the file opened okay, write to it:
   if (myFile) {
-  Serial.print("Writing to test.txt...");
-  digitalClockDisplay(myFile, cps);
-  // close the file:
-  myFile.close();
-  Serial.println("done.");
+    Serial.print("Writing to test.txt...");
+    digitalClockDisplay(myFile, cps);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
   } else {
-  // if the file didn't open, print an error:
-  Serial.println("1 error opening test.txt");
+    // if the file didn't open, print an error:
+    Serial.println("1 error opening test.txt");
   }
 }
 
 void readSDData(){
   myFile = SD.open("test.txt");
   if (myFile) {
-  Serial.println("test.txt:");
-    
-  // read from the file until there's nothing else in it:
-  while (myFile.available()) {
-    Serial.write(myFile.read());
-  }
-  // close the file:
-  myFile.close();
+    Serial.println("test.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
   } else {
-  // if the file didn't open, print an error:
-  Serial.println("2 error opening test.txt");
+    // if the file didn't open, print an error:
+    Serial.println("2 error opening test.txt");
   }
 }
 
@@ -125,15 +126,15 @@ void readSDData(){
 void digitalClockDisplay(File myFile, String cps) {
   // digital clock display of the time
   myFile.print(hour());
+  Serial.print(hour());
   printDigits(minute(), myFile);
   printDigits(second(), myFile);
-  // printDigits(millis(), myFile);
   myFile.print(","+cps);
   myFile.println();
+  Serial.print("\n");
 }
 
-time_t getTeensy3Time()
-{
+time_t getTeensy3Time() {
   return Teensy3Clock.get();
 }
 
@@ -144,12 +145,12 @@ unsigned long processSyncMessage() {
   unsigned long pctime = 0L;
   const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
 
-  if(Serial.find(TIME_HEADER)) {
-  pctime = Serial.parseInt();
-  return pctime;
-  if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
-    pctime = 0L; // return 0 to indicate that the time is not valid
-  }
+  if (Serial.find(TIME_HEADER)) {
+    pctime = Serial.parseInt();
+    return pctime;
+    if (pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+      pctime = 0L; // return 0 to indicate that the time is not valid
+    }
   }
   return pctime;
 }
@@ -157,8 +158,12 @@ unsigned long processSyncMessage() {
 void printDigits(int digits, File myFile){
   // utility function for digital clock display: prints preceding colon and leading 0
   myFile.print(":");
-  if(digits < 10)
-  myFile.print('0');
+  if(digits < 10) myFile.print('0');
   myFile.print(digits);
+
+  Serial.print(":");
+  if(digits < 10)
+  Serial.print('0');
+  Serial.print(digits);
 }
 
